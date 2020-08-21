b211a4886392222488ecbe2c4d1dd8089d38fb93
Dylan Araps <dylan.araps@gmail.com>
2020-08-21 10:54:20 +0300

    more initial action stuff

--- src/action.c
+++ src/action.c
@@ -3,3 +3,19 @@
 static void action_center(const union arg a) {
 
 }
+
+static void action_fullscreen(const union arg a) {
+
+}
+
+static void action_kill(const union arg a) {
+
+}
+
+static void action_workspace(const union arg a) {
+
+}
+
+static void action_workspace_send(const union arg a) {
+
+}
--- src/action.h
+++ src/action.h
@@ -1,4 +1,7 @@
 #include "globals.h"
 
 static void action_center(const union arg);
-
+static void action_fullscreen(const union arg);
+static void action_kill(const union arg);
+static void action_workspace(const union arg);
+static void action_workspace_send(const union arg);
